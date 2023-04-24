815ff8b5cd04842ef4d9b075539639a88c24a9d8
unknown <yogisdmec007@gmail.com>
2023-04-24 23:05:29 +0530

    code update

--- main.tf
+++ main.tf
@@ -86,10 +86,11 @@ module "eks_cluster_creation" {
   source = "terraform-aws-modules/eks/aws"
   version = "19.13.1"
   cluster_name                   = local.name
+  cluster_role = data.aws_iam_role.example.arn
   cluster_endpoint_public_access = true
   cluster_endpoint_private_access = false
   subnet_ids        =  flatten([aws_subnet.public-subnets[*].id])
-  cluster_role = data.aws_iam_role.example.arn
+  
   depends_on = [module.eks_nodegroup_role]
 }
 
