6b1ff797f1ee91e548ca67947ce756384b9de3f6
yogisdm <yogisdmec007@gmail.com>
2023-10-26 14:06:41 +0530

    code update

--- main.tf
+++ main.tf
@@ -160,10 +160,10 @@ source = "./node-group-creation"
 depends_on = [module.eks_cluster_creation]
 }
 
-//module "app_deployment"{
-//  source = "./eks_app_deployment"
-//  depends_on = [module.nodegroup_creation]
-//}
+module "app_deployment"{
+  source = "./eks_app_deployment"
+  depends_on = [module.nodegroup_creation]
+}
 
 //module "wordpress_db_deployment"{
 //  source = "./eks_wordpress_db_deployment"
